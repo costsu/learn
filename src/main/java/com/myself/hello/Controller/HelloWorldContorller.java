ea69931197f1cac7b74447d35f33adde244d1fc9
xiaozuliang <gamfz111@qq.com>
2022-07-10 10:29:44 +0800

    测试1

---
+++ src/main/java/com/myself/hello/Controller/HelloWorldContorller.java
@@ -0,0 +1,18 @@
+package com.myself.hello.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldContorller {
+
+    @GetMapping
+    public String helloworld(){
+        System.out.println(
+                "helloworld"
+        );
+        return "OK";
+    }
+}

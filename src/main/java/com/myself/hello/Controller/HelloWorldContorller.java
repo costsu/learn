e797cb56f4f8ab3994337c12d71c82c82fc070d3
xiaozuliang <gamfz111@qq.com>
2022-07-15 17:55:51 +0800

    test redis

--- src/main/java/com/myself/hello/Controller/HelloWorldContorller.java
+++ src/main/java/com/myself/hello/Controller/HelloWorldContorller.java
@@ -1,5 +1,7 @@
 package com.myself.hello.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloWorldContorller {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @GetMapping
     public String helloworld(){
-        System.out.println(
-                "helloworld。。。？"
-        );
+        redisTemplate.opsForValue().set("k1","v1");
+        String v1 = (String) redisTemplate.opsForValue().get("k1");
+        System.out.println(v1);
+
         return "OK";
     }
 }
